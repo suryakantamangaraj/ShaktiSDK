77689fdfdda498dc20f4780c4274fb93c613b347
sathya281 <sathya281@gmail.com>
2019-07-12 16:52:22 +0530

    change gpio offset

--- software/examples/gpio_applns/btnled/btnled.c
+++ software/examples/gpio_applns/btnled/btnled.c
@@ -3,7 +3,7 @@
 * Name of the file	     		:  btnled.c
 * Created date			        :  26.02.2019
 * Brief Description of file             :  Controls the led operation with help of button,gpio based.
-* Name of Author    	                :  Sathya Narayanan N
+* Name of Author    	                :  Sathya Narayanan N & Raghav
 * Email ID                              :  sathya281@gmail.com
 
     Copyright (C) 2019  IIT Madras. All rights reserved.
