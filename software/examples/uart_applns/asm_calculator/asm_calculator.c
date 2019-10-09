fb0555d9f4f0082d661252ebe74beeab30056deb
Abhinav Ramnath <abhinavramnath13@gmail.com>
2019-10-09 08:57:33 +0000

    Update asm_calculator.c

--- software/examples/uart_applns/asm_calculator/asm_calculator.c
+++ software/examples/uart_applns/asm_calculator/asm_calculator.c
@@ -1,3 +1,28 @@
+ /***************************************************************************
+* Project           			        :  shakti devt board
+* Name of the file	     		        :  asm_calculator.c
+* Brief Description of file             :  A calculator program written in asm.
+* Name of Author    	                :  Abhinav Ramnath
+* Email ID                              :  abhinavramnath13@gmail.com
+
+    Copyright (C) 2019  IIT Madras. All rights reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+***************************************************************************/
+
+
 /** @fn int getchar()
  * @brief getchar function uses UART to accept one character from input
  *
